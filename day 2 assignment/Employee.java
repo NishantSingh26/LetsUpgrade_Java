57bf2b13d9fa06c52e249f5938f682c4f6673dd9
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-10-11 20:47:55 +0530

    Add files via upload

---
+++ day 2 assignment/Employee.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Employee {
+
+    public String name;
+    public int age;
+    public String city;
+    public void display(){
+        System.out.println("The Name is "+name);
+        System.out.println("The Age is "+age);
+        System.out.println("The City is "+city);
+    }
+}
