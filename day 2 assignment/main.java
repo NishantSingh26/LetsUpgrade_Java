a14c30a143bcc25075b55cc292a4bbffaecf5166
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-10-11 20:43:05 +0530

    Create main.java

---
+++ day 2 assignment/main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee e=new Employee();
+        e.name="Saurabh";
+        e.age= 23;
+        e.city="Chennai";
+        e.display();
+
+        Employee e1=new Employee();
+        e1.name="Nishant";
+        e1.age= 19;
+        e1.city="Mumbai";
+        e1.display();
+        
+    }
+}
