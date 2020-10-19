100dc0ae6b210a754c1b3d672bc8c53fd8fcdced
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-10-19 20:05:36 +0530

    Create main.java

---
+++ project/main.java
@@ -0,0 +1,11 @@
+package com.quizappication;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code
+        Game game=new Game();
+        game.initGame();
+        game.play();
+    }
+}
