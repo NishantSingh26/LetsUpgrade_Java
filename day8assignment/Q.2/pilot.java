bf8a64d1c5ec4e8ee25df46f1c98522fa57c3cfa
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-11-01 21:31:43 +0530

    Create pilot.java

---
+++ day8assignment/Q.2/pilot.java
@@ -0,0 +1,10 @@
+package com.second;
+
+public class pilot extends Employee {
+    public void workplace(){
+        System.out.println("Pilots rarely adhere to the standard 50hour work week");
+    }
+    public void leave(){
+        System.out.println("Pilot have leave of 4 days");
+    }
+}
