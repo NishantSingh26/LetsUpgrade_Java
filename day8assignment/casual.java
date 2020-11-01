72fea8d7501348d5a169e208c230ff5bed2685e7
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-11-01 21:28:14 +0530

    Create casual.java

---
+++ day8assignment/casual.java
@@ -0,0 +1,7 @@
+package com.second;
+
+public class casual extends Employee{
+    public void no_regular_hrs(){
+        System.out.println("Casual Employee don't have Guarantee of regular working hours");
+    }
+}
