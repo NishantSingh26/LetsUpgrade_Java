f770c5da50cec96e63914ba2b59dfb6feddcc98c
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-11-01 21:31:14 +0530

    Create doctor.java

---
+++ day8assignment/Q.2/doctor.java
@@ -0,0 +1,14 @@
+package com.second;
+
+public class doctor extends Employee{
+    public void diagnose(){
+        System.out.println("Doctor find out the exact illness of person");
+    }
+    public void injury(){
+        System.out.println(" Doctors examine patients and perform surgeries");
+    }
+    public void leave(){
+        System.out.println("Doctor has leave of 1 days");
+    }
+
+}
