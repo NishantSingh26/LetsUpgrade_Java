63d6a5b876d5a3ee08e9b40f4ff37f4e72ba07e7
NishantSingh26 <72688322+NishantSingh26@users.noreply.github.com>
2020-11-01 21:27:43 +0530

    Create permanent.java

---
+++ day8assignment/permanent.java
@@ -0,0 +1,8 @@
+package com.second;
+public class permanent extends Employee {
+    public void standard_day(){
+        System.out.println("Can work of Standard Day i.e Monday to Friday 11am to 6pm");
+    }
+    
+
+}
